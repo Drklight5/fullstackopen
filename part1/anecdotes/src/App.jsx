4e825afc08cc3eb0035dc2cf179559807f4d9f36
Valeria <valeria050603@hotmail.com>
2023-09-01 10:10:08 -0600

    Part1 completed

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,61 +1,58 @@
-const Part = (props) => {
-  return(
-    <p>
-        {props.part} {props.exercises}
-    </p>
-  )
-}
-const Header = (props) =>{
-  console.log(props)
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-const Content = (props) =>{ 
- 
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}></Part>
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}></Part>
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}></Part>
-    </div>
-  )
-}
-const Total = (props) =>{
-  
-  return (
-    <div>
-       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </div>
-  )
-}
+import { useState } from 'react'
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass parts',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
   ]
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const [maxIndex,setMaxIndex] = useState(0);
 
+  const randomIndex = (max)=> Math.floor(Math.random() * Math.floor(max))
+  const setMax = () => {
+    let mx = Math.max(...votes);
+    let ind = (votes.indexOf(mx))
+    setMaxIndex(ind);
+  }
+  const nextAnec = () => setSelected(randomIndex(anecdotes.length))
+
+  const voteThis = ()=> {
+    let copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+    setMax();
+  }
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content parts = {parts}></Content>
-      <Total parts = {parts}></Total>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={anecdotes[selected]} votes = {votes[selected]}></Anecdote>
+      <button onClick={voteThis}>Vote</button>
+      <button onClick={nextAnec}>Next Anecdote</button>
+
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <Anecdote anecdote={anecdotes[maxIndex]} votes = {votes[maxIndex]}></Anecdote>
+      </div>
     </div>
   )
 }
+
+const Anecdote = ({anecdote, votes}) => {
+  return (
+  <div>
+    <p>{anecdote}</p>
+    <p>has {votes} votes</p>
+  </div>
+  )
+}
+
 
 export default App
--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,61 +1,81 @@
-const Part = (props) => {
-  return(
-    <p>
-        {props.part} {props.exercises}
-    </p>
-  )
-}
-const Header = (props) =>{
-  console.log(props)
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [stats, setStats] = useState({
+    good:0,
+    neutral:0,
+    bad:0
+  })
+
+  
+  const GoodRev =  ()=> {setStats({...stats, good: stats.good+1})}
+  const NeutralRev =  ()=> {setStats({...stats, neutral: stats.neutral+1})}
+  const BadRev =  ()=> {setStats({...stats, bad: stats.bad+1})}
   return (
     <div>
-      <h1>{props.course}</h1>
+
+      <h1>Give Feedback</h1>
+      <div>
+        <Button handle={GoodRev} text="Good"></Button>
+        <Button handle={NeutralRev} text="Neutral"></Button>
+        <Button handle={BadRev} text="Bad"></Button>
+      </div>
+      <Statistics stats ={stats}></Statistics>
+      
     </div>
   )
 }
-const Content = (props) =>{ 
- 
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}></Part>
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}></Part>
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}></Part>
-    </div>
-  )
-}
-const Total = (props) =>{
+
+const Statistics = ({stats})=>{
+  
+  const allStats = ()=> {return stats.good + stats.neutral + stats.bad}
+  function average() {
+    return ((stats.good - stats.bad)/(allStats()))
+  }
+  function positive() {
+    return ((stats.good*100)/(allStats()) +  " %")
+  }
+  if((allStats()) > 0){
+
   
   return (
     <div>
-       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text = "Good" value = {stats.good}></StatisticLine>
+          <StatisticLine text = "Neutral" value = {stats.neutral}></StatisticLine>
+          <StatisticLine text = "Bad" value = {stats.bad}></StatisticLine>
+          
+          <StatisticLine text = "All" value = {allStats()}></StatisticLine>
+          <StatisticLine text = "Average" value = {average()}></StatisticLine>
+          <StatisticLine text = "Positive" value = {positive()}></StatisticLine>
+        </tbody>
+      </table>
+
     </div>
-  )
-}
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass parts',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
-
+  )}
+  
   return (
     <div>
-      <Header course={course}></Header>
-      <Content parts = {parts}></Content>
-      <Total parts = {parts}></Total>
+      <h1>Statistics</h1>
+      <p>No feedback given</p>
     </div>
   )
 }
 
+const StatisticLine = ({text, value})=>{
+  return(
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td> 
+    </tr>
+  )
+}
+const Button  = ({handle, text}) =>{
+  return (
+    <button onClick={handle}>{text}</button>
+  )
+}
 export default App
